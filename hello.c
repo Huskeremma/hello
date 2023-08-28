087775a116d2e02bf2eaf5fe703fba7bc57e0fd2
Huskeremma <142922902+Huskeremma@users.noreply.github.com>
2023-08-28 09:43:11 -0500

    added exclamations

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
  * Author: Emma Walter
  * Date: 2023/08/22
  *
- * hello world program in C <3
+ * hello world program in C
  *
 */
 #include <stdlib.h>
@@ -10,10 +10,11 @@
 
 int main(int argc, char **argv){
 
+
     printf("Emma Walter!");
 
-    printf("I'm a computer engineering major");
 
+    printf("I'm a computer engineering major!!!");
 
 
     return 0;
